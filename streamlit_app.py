f8091958978db60ab600c7e4268dbb9e591a7bc0
RAVI SHEKHAR TIWARI <tiwari11.rst@gmail.com>
2021-12-31 14:40:20 +0530

    header aded

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,71 @@ st.title("Logistic Regression - Mnist Dataset")
 
 #%%
 #Navigation bar
+with st.beta_container():
+ #navbar 
+#https://bootsnipp.com/snippets/nNX3a     https://www.mockplus.com/blog/post/bootstrap-navbar-template
+   components.html(
+       """
+       <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark" >
+  <a class="navbar-brand" href="https://www.rstiwari.com" target="_blank">Profile</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href=" https://tiwari11-rst.medium.com/" target="_blank" >
+          <i class="fa fa-home"></i>
+          Medium
+          <span class="sr-only">(current)</span>
+          </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=" https://happyman11.github.io/" target="_blank">
+          <i class="fa fa-envelope-o">
+            <span class="badge badge-danger">Git Pages</span>
+          </i>
+         
+        </a>
+      </li>
+      
+        <li class="nav-item">
+        <a class="nav-link" href="https://happyman11.github.io/" target="_blank">
+          <i class="fa fa-globe">
+            <span class="badge badge-success">Badges</span>
+          </i>
+         
+        </a>
+      </li>
+          
+        </a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link disabled" href="https://ravishekhartiwari.blogspot.com/" target="_blank">
+          <i class="fa fa-envelope-o">
+            <span class="badge badge-warning">Blogspot</span>
+          </i>
+          
+        </a>
+      </li>
+      
+      
+    </ul>
+  
+    
+  </div>
+</nav>
+       """, height=70,
+    )
 
 
 
-
-
+###
 st.sidebar.markdown("""** Hyperparameter Selection **""")
 Split = st.sidebar.slider('Train-Test Splitup (in %)', 0.1,0.9,.70)
 st.sidebar.markdown("""**Select SVM Parameters**""")
